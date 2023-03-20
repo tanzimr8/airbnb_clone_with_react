eaa7fdce6a420d0b7104a760ddb5346602af47fb
Tanzim <tanzim008@gmail.com>
2023-03-20 01:28:39 -0230

    Main section draft

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,22 @@ const Main = ()=>{
     return (
         <div className="Main">
             <div className='container'>
-                <p>I am <code>Tanzim</code> and I develop website.</p>
+                <div className="pre-title">
+                    Hello! I am
+                </div>
+                <div className="title">
+                    <h2>Tanzim Rahman</h2>
+                </div>
+                <div className="sub-title">
+                    <ul>
+                        <li>Traveler</li>
+                        <li>Front end Developer</li>
+                        <li>Customer Service</li>
+                    </ul>
+                </div>
+                <div className="description">
+                    <p>Serving as Customer service Representative at Intact. Also training myself with frontend technology such as JavaScript, React JS and UX design</p>
+                </div>
             </div>
         </div>
     )
