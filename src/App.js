f21142d5a5ad2ce3f1bdd1a8918d64c40c476824
Tanzim <tanzim008@gmail.com>
2023-03-20 00:45:11 -0230

    first react commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Navbar/>
+          <Main/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+const Main = ()=>{
+    return (
+        <div>
+            <p>I am <code>Tanzim</code> and I develop website.</p>
+            <a
+                className="App-link"
+                href="https://tanzim-rahman.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Visit my website
+            </a>
+        </div>
+    )
+}
+export default Main;
